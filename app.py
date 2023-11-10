0ea37b281560050dcacbe441c869b3661fd5ab0b
unknown <saminathan.harshini@divum.in>
2023-11-10 17:01:06 +0530

    emailverification added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from functools import wraps
+import random
+import smtplib
 
 import jwt
 from flask import Flask, render_template, url_for, redirect, request, flash, jsonify
@@ -34,10 +37,10 @@ def signup():
             con.execute(sql, (username, password, fullname))
             mysql.connection.commit()
             con.close()
-            expiration_time = datetime.utcnow() + timedelta(hours=1)
-            token = jwt.encode({'username': username, 'exp': expiration_time},
-                               SECRET_KEY, algorithm='HS256')
-            return jsonify(success=True, token=token)
+            # expiration_time = datetime.utcnow() + timedelta(hours=1)
+            # token = jwt.encode({'username': username, 'exp': expiration_time},
+            #                    SECRET_KEY, algorithm='HS256')
+            return jsonify(success=True)
 
         else:
             return jsonify(error='username already exists')
@@ -58,14 +61,31 @@ def login():
     print(result['count'], "result")
     con.close()
     if result['count'] == 1:
-        expiration_time = datetime.utcnow() + timedelta(hours=1)
-        token = jwt.encode({'username': username, 'exp': expiration_time},
+        token = jwt.encode({'username': username},
                            SECRET_KEY, algorithm='HS256')
         return jsonify(success=True, token=token)
     else:
         return jsonify(success=False, error='Invalid credentials')
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify(success=False, error='Token is missing'), 401
+
+        try:
+            data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            username = data['username']
+
+        except jwt.InvalidTokenError:
+            return jsonify(success=False, error='Invalid token'), 401
+        return f(username, *args, **kwargs)
+
+    return decorated
+
 @app.route("/display/<username>", methods=['GET'])
+@token_required
 def display(username):
     try:
         con = mysql.connection.cursor()
@@ -81,6 +101,85 @@ def display(username):
     except Exception as e:
         print(e)
         return jsonify(success=False, error='DB error')
+@app.route("/usersignup", methods=['POST'])
+def usersignup():
+    try:
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        email=data['email']
+        phonenumber=data['phonenumber']
+        con = mysql.connection.cursor()
+        sql1 = "SELECT count(*) as count FROM userinfo WHERE username = %s"
+        con.execute(sql1, (username,))
+        usernamecount = con.fetchone()
+        print(usernamecount['count'], "result")
+        con.close()
+        if usernamecount['count'] < 1:
+            con = mysql.connection.cursor()
+            sql = "INSERT INTO userinfo (username, password, email,phonenumber) VALUES (%s, %s, %s,%s)"
+            con.execute(sql, (username, password, email,phonenumber))
+            mysql.connection.commit()
+            con.close()
+            return jsonify(success=True)
+        else:
+            return jsonify(error='username already exists')
+    except Exception as e:
+        print(e)
+        return jsonify(error='DB error')
+
+
+global_otp= None
+def generate_otp():
+    return str(random.randint(10000, 99999))
+
+@app.route("/forgotpasswordotp", methods=['GET'])
+def forgotpasswordotp():
+    try:
+        global global_otp
+        data = request.get_json()
+        email = data['email']
+        otp = generate_otp()
+        global_otp=otp
+        print(global_otp)
+        send_otp_email(email, otp)
+        return jsonify(success=True, message='OTP sent successfully')
+    except Exception as e:
+        print(e)
+        return jsonify(error='Failed to send OTP')
+
+@app.route("/otpverify",methods=['GET'])
+def otpverify():
+    try:
+        data=request.get_json()
+        email=data['email']
+        otp=data['otp']
+        print(global_otp,otp)
+        if otp==global_otp:
+            return jsonify(message='OTP VALID',success=True)
+        else:
+            return jsonify(message='OTP INVALID',success=False)
+    except Exception as e:
+        return jsonify(error='Failed to verify')
+
+def send_otp_email(email, otp):
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = 'sharshini2003@gmail.com'
+    smtp_password = 'znwm mgfw jaxc bdyp'
+    from_email = 'sharshini2003@gmail.com'
+    to_email = email
+
+    subject = 'OTP Verification'
+    body = f'Your OTP is: {otp}. \n This is to verify your account for FORGOT-PASSWORD-VERIFICATION. \n\n Note: This is an auto-generated mail. Don\'t reply to this email.'
+
+    message = f'Subject: {subject}\n\n{body}'
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(from_email, to_email, message)
+
 
 
 if (__name__ == '__main__'):
